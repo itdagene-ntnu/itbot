42bc8cac648f88052f3f58d1b069a6790b737565
Peder Smith <smith.peder@gmail.com>
2018-09-08 20:00:31 +0200

    Add helper command for roles

--- scripts/roles.js
+++ scripts/roles.js
@@ -3,6 +3,7 @@
 // Commands
 //  @<role> ... - Make itbot mention all users with that role
 //  @<role> some text @<another role> ... - Make itbot mention multiple roles
+//  itbot roles ... - itbot tells you all roles in the current system
 
 const _ = require("lodash");
 
@@ -21,5 +22,19 @@ module.exports = robot => {
       .map(person => createMention(person.slack))
       .join(", ");
     msg.send(`${mentions}`);
+  });
+
+  robot.respond(/roles/i, msg => {
+    const roles = Object.keys(
+      members
+        .map(member => member.role)
+        .reduce((obj, a) => ({ [a]: true, ...obj }), {})
+    )
+      .map(role => `    - ${role}`)
+      .sort()
+      .join("\n");
+    msg.send(
+      `You can use @<role> to mention users.\nUsers in this workspace have these roles:\n${roles}`
+    );
   });
 };
