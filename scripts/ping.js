287e3ee6c328f45821d6ae1afb44135151a75bc5
Peder Smith <smith.peder@gmail.com>
2018-09-09 23:05:12 +0200

    Try to reboot CI with new push

--- scripts/ping.js
+++ scripts/ping.js
@@ -1,8 +1,10 @@
 // Description:
 // Test itbot
 
+const time = new Date().getTime();
+
 module.exports = robot => {
   robot.respond(/ping/i, msg => {
-    msg.reply(`pong`);
+    msg.reply(`pong ${time}`);
   });
 };
